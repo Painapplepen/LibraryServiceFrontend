a8ed9be3c9aae08870f4c685fdff20597d297015
Anton <zditovets55@gmail.com>
2021-04-08 16:44:20 +0300

    update navbar, add auth interceptor, add styles, delete bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,12 @@ import { PublishersPageComponent } from './content/pages/publishers-page/publish
 import { HeaderComponent } from './content/layout/header/header.component';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FooterComponent } from './content/layout/footer/footer.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthGuard } from './core/guards/auth.guard';
+import { AuthService } from './core/auth/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginPageComponent } from './content/pages/login-page/login-page.component';
 
 @NgModule({
   declarations: [
@@ -17,6 +23,7 @@ import { FooterComponent } from './content/layout/footer/footer.component';
     BooksPageComponent,
     BookfundsPageComponent,
     GenresPageComponent,
+    LoginPageComponent,
     LibrariesPageComponent,
     PublishersPageComponent,
     HeaderComponent,
@@ -25,9 +32,13 @@ import { FooterComponent } from './content/layout/footer/footer.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
     MDBBootstrapModule.forRoot()
   ],
-  providers: [],
+  providers: [AuthGuard, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/core/auth/auth.interseptor.ts
@@ -0,0 +1,39 @@
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { Router } from "@angular/router";
+import { catchError } from "rxjs/operators";
+import { AuthService } from './auth.service';
+
+@Injectable()
+export class AuthInterseptor implements HttpInterceptor {
+
+    constructor(private authService: AuthService,
+                private router: Router) {
+    }
+
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        if (this.authService.isAuthenticated()) {
+            req = req.clone({
+                setParams: {
+                    auth: this.authService.token
+                }
+            });
+        }
+        return next.handle(req).pipe(
+            catchError((error: HttpErrorResponse) => {
+                console.log('Interseptor error');
+                if (error.status === 401) {
+                    this.authService.logout();
+                    this.router.navigate(['/admin', 'login'], {
+                        queryParams: {
+                            authFailed: true
+                        }
+                    });
+                }
+                return throwError(error);
+            }));
+    }
+
+}
